0ecf782f81b8913f3a4275a35c65e9655119f205
Alberto Flores <alberto.flores@shipedge.com>
2026-03-02 09:25:10 -0400

    feat: add Swagger documentation and Prisma migrations

--- src/routes/search.routes.js
+++ src/routes/search.routes.js
@@ -10,6 +10,35 @@ router.use(requireVerifiedKYC);
 
 // Consultar garajes disponibles
 // Ejemplo: GET /api/search?fecha=2026-03-01&hora_inicio=10:00&hora_fin=14:00
+/**
+ * @swagger
+ * /api/search:
+ *   get:
+ *     summary: Search for available garages
+ *     tags: [Search]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: fecha
+ *         schema:
+ *           type: string
+ *           format: date
+ *         description: Date to search (YYYY-MM-DD)
+ *       - in: query
+ *         name: hora_inicio
+ *         schema:
+ *           type: string
+ *         description: Start time (HH:mm)
+ *       - in: query
+ *         name: hora_fin
+ *         schema:
+ *           type: string
+ *         description: End time (HH:mm)
+ *     responses:
+ *       200:
+ *         description: List of available garages
+ */
 router.get('/', searchGarajes);
 
 module.exports = router;
